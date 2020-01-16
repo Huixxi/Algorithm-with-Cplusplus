f8f6fcd51206e234fa59f8a1565147ea58e6f700
HU <huxixi.love.story@gmail.com>
2020-01-17 00:16:24 +0800

    Create UVa10943_How do you add?.cpp

---
+++ Week13-TSP旅行商&区间DP/UVa10943_How do you add?.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MODN 1000000
+
+int dp[101][101];  // dp[k][n]
+
+int main() {
+    for(int k = 1; k <= 100; ++k) {
+        dp[k][0] = 1;
+        for(int n = 1; n <= 100; ++n) {
+            dp[k][n] = (dp[k-1][n] + dp[k][n-1]) % MODN;
+        }
+    }
+
+    int N, K;
+    while(cin >> N >> K && N + K) {
+        cout << dp[K][N];
+    }
+    return 0;
+}
