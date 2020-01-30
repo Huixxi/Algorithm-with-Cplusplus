e74acad34e35ea2d4635cd0c9d3b1e5d4f182a28
HU <huxixi.love.story@gmail.com>
2020-01-30 19:11:07 +0800

    Create STL_Stack.cpp

---
+++ Week04-STL/STL_Stack.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+	  stack<int> sta;  // LIFO(last in first out)
+	  for(int i = 0; i < 10; ++i)
+        sta.push(i);
+    while(!sta.empty()){
+	      int no = sta.top();
+        sta.pop();
+	      cout << no << endl;
+    }
+	  return 0;
+}
