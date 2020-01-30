c91fedc82cee08a296387aa213587e1114c5857c
HU <huxixi.love.story@gmail.com>
2020-01-30 19:14:35 +0800

    Update STL_Stack.cpp

--- Week04-STL/STL_Stack.cpp
+++ Week04-STL/STL_Stack.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main(){
-	  stack<int> sta;  // LIFO(last in first out)
-	  for(int i = 0; i < 10; ++i)
+    stack<int> sta;  // LIFO(last in first out)
+    for(int i = 0; i < 10; ++i)
         sta.push(i);
     while(!sta.empty()){
-	      int no = sta.top();
+	int no = sta.top();
         sta.pop();
-	      cout << no << endl;
+	cout << no << endl;
     }
-	  return 0;
+    return 0;
 }
