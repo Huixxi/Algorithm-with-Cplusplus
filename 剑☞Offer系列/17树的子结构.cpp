d16dd3b4c106d0ed6afbe7ebb7fb6501787bcdd9
HU <huxixi.love.story@gmail.com>
2020-02-05 22:34:25 +0800

    Create 17树的子结构.cpp

---
+++ 剑☞Offer系列/17树的子结构.cpp
@@ -0,0 +1,29 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    bool HasSubtree(TreeNode* pRoot1, TreeNode* pRoot2)
+    {
+        if(!pRoot1 || !pRoot2)
+            return false;
+        return dfs(pRoot1, pRoot2) || HasSubtree(pRoot1->left, pRoot2) || HasSubtree(pRoot1->right, pRoot2);
+    }
+private:
+    bool dfs(TreeNode* pRoot1, TreeNode* pRoot2) {
+        if(!pRoot2)
+            return true;
+        if(!pRoot1)
+            return false;
+        if(pRoot1->val == pRoot2->val)
+           return dfs(pRoot1->left, pRoot2->left) && dfs(pRoot1->right, pRoot2->right);
+        else
+           return false;
+    }
+};
