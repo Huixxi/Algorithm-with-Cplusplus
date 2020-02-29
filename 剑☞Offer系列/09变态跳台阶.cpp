b31840e0d8dc40ea79d92a20c015b508d738fcaa
HU <huxixi.love.story@gmail.com>
2020-02-29 13:25:10 +0800

    Update 09变态跳台阶.cpp

--- 剑☞Offer系列/09变态跳台阶.cpp
+++ 剑☞Offer系列/09变态跳台阶.cpp
@@ -5,4 +5,18 @@ public:
             return 1;
         return 2 * jumpFloorII(number - 1);
     }
+    // 循环解法
+    /*
+    int jumpFloorII(int number) {
+        vector<int> res;
+        res.push_back(1);
+        for(int i = 1; i < number; ++i) {
+            int sum = 1;
+            for(int j = i - 1; j >= 0; --j)
+                sum += res[j];
+            res.push_back(sum);
+        }
+        return res[number-1];
+    }
+    */
 };
