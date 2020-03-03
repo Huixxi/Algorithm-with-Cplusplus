07d5bf00f3a32857a7f063894c686fa9dff52b0b
HU <huxixi.love.story@gmail.com>
2020-03-03 12:48:02 +0800

    Update 40数组中只出现一次的数字.cpp

--- 剑☞Offer系列/40数组中只出现一次的数字.cpp
+++ 剑☞Offer系列/40数组中只出现一次的数字.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void FindNumsAppearOnce(vector<int> data,int* num1,int *num2) {
+        /*
         // 先排个序 O(nlogn)
         sort(data.begin(), data.end());
         int flag = 0;
@@ -23,5 +24,25 @@ public:
                 return;
         }
         *num2 = data[data.size() - 1];
+        */
+        // Update
+        map<int, int> mm;
+        for(int& i : data) {
+            if(mm.find(i) != mm.end())
+                ++mm[i];
+            else
+                mm[i] = 1;
+        }
+        int c = 0;
+        for(int& i : data) {
+            if(c == 0 && mm[i] == 1) {
+                *num1 = i;
+                ++c;
+            }
+            else if(c == 1 && mm[i] == 1) {
+                *num2 = i;
+                break;
+            }
+        }
     }
 };
