db70577398c5cdbaa54a59ea4553490cfda0b658
HU <huxixi.love.story@gmail.com>
2020-03-06 21:45:13 +0800

    Update 42和为S的两个数字.cpp

--- 剑☞Offer系列/题目41-50/42和为S的两个数字.cpp
+++ 剑☞Offer系列/题目41-50/42和为S的两个数字.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     vector<int> FindNumbersWithSum(vector<int> array,int sum) {
         vector<int> res;
-        for(int i : array) {
-            for(int j = array.size() - 1; array[j] >= sum - i; --j) {
-                if(array[j] == sum - i) {
-                    res.push_back(i);
-                    res.push_back(array[j]);
-                    return res;
-                }
+        int i = 0, j = array.size() - 1;
+        while(i < j) {
+            if(array[j] == sum - array[i]) {
+                res.push_back(array[i]);
+                res.push_back(array[j]);
+                return res;
             }
-        }和为S的两个数字
+            if(array[j] > sum - array[i])
+                --j;
+            else
+                ++i;
+        }
+        return res;
     }
 };
