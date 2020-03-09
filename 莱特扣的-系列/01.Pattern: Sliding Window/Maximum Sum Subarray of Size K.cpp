e8d7dde7554888dd0abbca08a784f2b369bf3739
HU <huxixi.love.story@gmail.com>
2020-03-09 13:03:19 +0800

    Update Maximum Sum Subarray of Size K.cpp

--- 莱特扣的-系列/01.Pattern: Sliding Window/Maximum Sum Subarray of Size K.cpp
+++ 莱特扣的-系列/01.Pattern: Sliding Window/Maximum Sum Subarray of Size K.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int maxSum(vector<int> nums, int k) {
+    if(k > nums.size())
+        return -1;
+    int j = k, msum = 0;
+    for(int t = 0; t < k; ++t)
+        msum += nums[t];
+    while(j < nums.size()) {
+        msum = max(msum, msum + (nums[j] - nums[j - k]));
+        ++j;
+    }
+    return msum;
+}
