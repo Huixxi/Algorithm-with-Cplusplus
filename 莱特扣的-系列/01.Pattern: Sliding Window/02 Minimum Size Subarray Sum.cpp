2a25319a3b2f26c9968ba4284422b8e9f7de2ee6
HU <huxixi.love.story@gmail.com>
2020-03-10 11:32:26 +0800

    Update 02 Minimum Size Subarray Sum.cpp

--- 莱特扣的-系列/01.Pattern: Sliding Window/02 Minimum Size Subarray Sum.cpp
+++ 莱特扣的-系列/01.Pattern: Sliding Window/02 Minimum Size Subarray Sum.cpp
@@ -1,18 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int minSubArrayLen(int s, vector<int>& nums) {
-    int i = 0, j = 0, sum = 0, l = 10000;
-    if(nums.size() == 0)
-        return 0;
-    while(j < nums.size()) {
-        while(sum < s && j < nums.size()) 
-            sum += nums[j++];
-        if(sum < s && l == 10000)
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int i = 0, j = 0, sum = 0, l = 10000;
+        if(nums.size() == 0)
             return 0;
-        while(sum >= s) 
-            sum -= nums[i++];
-        l = min(l, j - i + 1);
+        while(j < nums.size()) {
+            while(sum < s && j < nums.size()) 
+                sum += nums[j++];
+            if(sum < s && l == 10000)
+                return 0;
+            while(sum >= s) 
+                sum -= nums[i++];
+            l = min(l, j - i + 1);
+        }
+        return l;
     }
-    return l;
-}
+};
