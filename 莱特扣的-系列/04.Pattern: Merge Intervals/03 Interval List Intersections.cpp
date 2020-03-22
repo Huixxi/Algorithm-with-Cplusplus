e19a2f5b6561d9322884305895d82d11c51c0ad1
HU <huxixi.love.story@gmail.com>
2020-03-22 14:59:22 +0800

    Update 03 Interval List Intersections.cpp

--- 莱特扣的-系列/04.Pattern: Merge Intervals/03 Interval List Intersections.cpp
+++ 莱特扣的-系列/04.Pattern: Merge Intervals/03 Interval List Intersections.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
         vector<vector<int>> res;
-        if(A.size() == 0 || B.size() == 0)
-            return res;
         int i = 0, j = 0;
         while(i < A.size() && j < B.size()) {
-            if(A[i][0] > B[j][1]) {
-                ++j;
-                continue;
+            int lo = max(A[i][0], B[j][0]);
+            int hi = min(A[i][1], B[j][1]);
+            if(lo <= hi) {
+                vector<int> temp{lo, hi};
+                res.push_back(temp);
             }
-            if(A[i][1] < B[j][0]) {
-                ++i;
-                continue;
-            }
-            vector<int> temp{max(A[i][0], B[j][0]), min(A[i][1], B[j][1])};
-            res.push_back(temp);
             if(A[i][1] <= B[j][1]) 
                 ++i;
             else
