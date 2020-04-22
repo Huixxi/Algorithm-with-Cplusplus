6626483d914115cd568d8ab9d02eddc90dad8090
HU <huxixi.love.story@gmail.com>
2020-04-22 10:56:25 +0800

    Update 6. Search in a Sorted Infinite Array.cpp

--- 莱特扣的-系列/11.Pattern: Modified Binary Search/6. Search in a Sorted Infinite Array.cpp
+++ 莱特扣的-系列/11.Pattern: Modified Binary Search/6. Search in a Sorted Infinite Array.cpp
@@ -3,11 +3,10 @@ using namespace std;
 
 int binarySearch(int arr[], int key) {
     // find the lower_bound and upper_bound
-    int val = arr[1], start = 0, end = 1;
-    while(val < key) {
+    int start = 0, end = 1;
+    while(arr[end] < key) {
         start = end;
         end = 2 * start;
-        val = arr[end];
     }
     while(start <= end) {
         // the safest way to find the middle of two numbers without getting an overflow
