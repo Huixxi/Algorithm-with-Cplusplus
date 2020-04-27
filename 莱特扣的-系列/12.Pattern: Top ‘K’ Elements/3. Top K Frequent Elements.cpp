775a962748b96e0fdbe8c60799168ec9c148d118
HU <huxixi.love.story@gmail.com>
2020-04-27 18:00:48 +0800

    Update 3. Top K Frequent Elements.cpp

--- 莱特扣的-系列/12.Pattern: Top ‘K’ Elements/3. Top K Frequent Elements.cpp
+++ 莱特扣的-系列/12.Pattern: Top ‘K’ Elements/3. Top K Frequent Elements.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mm;
+        for(auto& n : nums) {
+            ++mm[n];
+        }
+        priority_queue< pair<int, int>, vector<pair<int, int>>, greater_comp > pq;
+        for(auto& x : mm) {
+            pq.push(x);
+            if(pq.size() > k)
+                pq.pop();
+        }
+        vector<int> res;
+        while(!pq.empty()) {
+            res.push_back(pq.top().first);
+            pq.pop();
+        }
+        return res;
+    }
+    
+    struct greater_comp {
+        bool operator() (pair<int, int>& a, pair<int, int>& b) {
+            return a.second > b.second;
+        }  
+    };
+};
