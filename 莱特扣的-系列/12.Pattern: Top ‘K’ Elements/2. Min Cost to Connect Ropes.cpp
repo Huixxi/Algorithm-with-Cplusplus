7339f0ca5fb07346ffca3fba0fe33b9d91440085
HU <huxixi.love.story@gmail.com>
2020-04-27 15:39:54 +0800

    Update 2. Min Cost to Connect Ropes.cpp

--- 莱特扣的-系列/12.Pattern: Top ‘K’ Elements/2. Min Cost to Connect Ropes.cpp
+++ 莱特扣的-系列/12.Pattern: Top ‘K’ Elements/2. Min Cost to Connect Ropes.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int minCost(vector<int>& ropes) {
+        int tolCost = 0;  
+        priority_queue<int, vector<int>, greater<>> pq;  // min-heap
+        for(auto& rope : ropes)
+            pq.push(rope);
+        while(pq.size() > 1) {
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            tolCost += a + b;
+            pq.push(a + b);
+        }
+        return tolCost;
+    }
+};
