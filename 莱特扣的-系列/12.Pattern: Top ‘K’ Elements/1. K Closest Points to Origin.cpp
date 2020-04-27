77b8944c85156fe2e762069477f7f0b37b40a8f0
HU <huxixi.love.story@gmail.com>
2020-04-27 12:23:24 +0800

    Update 1. K Closest Points to Origin.cpp

--- 莱特扣的-系列/12.Pattern: Top ‘K’ Elements/1. K Closest Points to Origin.cpp
+++ 莱特扣的-系列/12.Pattern: Top ‘K’ Elements/1. K Closest Points to Origin.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        vector<vector<int>> res;
+        priority_queue<vector<int>, vector<vector<int>>, comp> pq;
+        for(auto& p : points) {
+            pq.push(p);
+            if(pq.size() > K)
+                pq.pop();
+        }
+        while(!pq.empty()) {
+            res.push_back(pq.top());
+            pq.pop();
+        }
+        return res;
+    }
+    
+    struct comp {
+        bool operator() (vector<int>& a, vector<int>& b) {
+            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
+        }
+    };  
+};
