ee05216d7377af206646dd47927fcde80abde684
HU <huxixi.love.story@gmail.com>
2020-04-27 18:24:02 +0800

    Update 4. Sort Characters By Frequency.cpp

--- 莱特扣的-系列/12.Pattern: Top ‘K’ Elements/4. Sort Characters By Frequency.cpp
+++ 莱特扣的-系列/12.Pattern: Top ‘K’ Elements/4. Sort Characters By Frequency.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    string frequencySort(string s) {
+        int l = s.length();
+        unordered_map<char, int> mm;
+        for(auto& c : s)
+            ++mm[c];
+        priority_queue< pair<char, int>, vector<pair<char, int>>, less_comp > pq;
+        for(auto& x : mm) 
+            pq.push(x);
+        
+        string res;
+        while(!pq.empty()) {
+            for(int i = 0; i < pq.top().second; ++i)
+                res += pq.top().first;
+            pq.pop();
+        }
+        return res;
+    }
+    
+    struct less_comp {
+        bool operator() (pair<char, int>& a, pair<char, int>& b) {
+            return a.second < b.second;
+        }  
+    };
+};
