79d85dd94a96e282d28cfd38fac9809aa41da418
HU <huxixi.love.story@gmail.com>
2020-04-27 20:25:16 +0800

    Update 6. Find K Closest Elements.cpp

--- 莱特扣的-系列/12.Pattern: Top ‘K’ Elements/6. Find K Closest Elements.cpp
+++ 莱特扣的-系列/12.Pattern: Top ‘K’ Elements/6. Find K Closest Elements.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int l = 0, r = arr.size() - k;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(x - arr[mid] > arr[mid + k] - x)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return vector<int>(arr.begin() + l, arr.begin() + l + k);
+    }
+};
