1ac17c1eda2415dc7672d179f442b4573065c55f
HU <huxixi.love.story@gmail.com>
2020-05-23 13:20:25 +0800

    Update 19顺时针打印矩阵.cpp

--- 剑☞Offer系列/题目11-20/19顺时针打印矩阵.cpp
+++ 剑☞Offer系列/题目11-20/19顺时针打印矩阵.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    vector<int> printMatrix(vector<vector<int> > matrix) {
-        int l = 0, r = matrix[0].size() - 1;
-        int t = 0, b = matrix.size() - 1;
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> res;
-        while(l < r && t < b) {
-            for(int i = l; i < r; ++i)
-                res.push_back(matrix[t][i]);
-            for(int i = t; i < b; ++i)
-                res.push_back(matrix[i][r]);
-            for(int i = r; i > l; --i)
-                res.push_back(matrix[b][i]);
-            for(int i = b; i > t; --i)
-                res.push_back(matrix[i][l]);
-            ++l;
-            --r;
-            ++t;
-            --b;
+        int nr = matrix.size(); 
+        if(nr == 0) return res;
+        int nc = matrix[0].size();
+        if(nc == 0) return res;
+        vector<vector<int>> dirs{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        vector<int> nSteps{nc, nr - 1};
+        int idir = 0;
+        int ir = 0, ic = -1;
+        while(nSteps[idir % 2]) {  // 偶数方向（左右）改变ic，奇数方向（上下）改变ir
+            for(int i = 0; i < nSteps[idir % 2]; ++i) {
+                ir += dirs[idir][0];
+                ic += dirs[idir][1];
+                res.push_back(matrix[ir][ic]);
+            }
+            --nSteps[idir % 2];
+            idir = (idir + 1) % 4;
         }
-        if(l == r && t == b)
-            res.push_back(matrix[t][l]);
-        if(l == r && t < b)
-            for(int i = t; i <= b; ++i)
-                res.push_back(matrix[i][l]);
-        if(l < r && t == b)
-            for(int i = l; i <= r; ++i)
-                res.push_back(matrix[t][i]);
         return res;
     }
 };
