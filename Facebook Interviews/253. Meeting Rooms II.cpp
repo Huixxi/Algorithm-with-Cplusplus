327952d3ded27347f01db4e2c1cfc85d99e90d08
HU <huxixi.love.story@gmail.com>
2020-06-11 21:53:30 +0800

    Create 253. Meeting Rooms II.cpp

---
+++ Facebook Interviews/253. Meeting Rooms II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        map<int, int> m;
+        for (auto a : intervals) {
+            ++m[a[0]];
+            --m[a[1]];
+        }
+        int rooms = 0, res = 0;
+        for (auto it : m) {
+            res = max(res, rooms += it.second);
+        }
+        return res;
+    }
+};
