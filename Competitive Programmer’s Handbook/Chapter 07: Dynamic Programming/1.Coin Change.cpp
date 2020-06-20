04bb7403bd28fbccdb2b082a978540d77a30ea1a
HU <huxixi.love.story@gmail.com>
2020-06-20 17:07:12 +0800

    Update 1.Coin Change.cpp

--- Competitive Programmer’s Handbook/Chapter 07: Dynamic Programming/1.Coin Change.cpp
+++ Competitive Programmer’s Handbook/Chapter 07: Dynamic Programming/1.Coin Change.cpp
@@ -4,9 +4,9 @@ public:
         vector<int> dp(amount + 1, 10000);
         dp[0] = 0;
         for(int a = 1; a <= amount; ++a) {
-            for(int i = 0; i < coins.size(); ++i) {
-                if(a - coins[i] >= 0)
-                    dp[a] = min(dp[a - coins[i]] + 1, dp[a]);
+            for(auto c : coins) {
+                if(a - c >= 0)
+                    dp[a] = min(dp[a - c] + 1, dp[a]);
             }
         }
         return dp[amount] == 10000 ? -1 : dp[amount];        
