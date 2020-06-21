2f0bd4a1609335d3f1d61a48c5043c9a65883247
HU <huxixi.love.story@gmail.com>
2020-06-21 17:07:01 +0800

    Create 4.Minimum Path Sum.cpp

---
+++ Competitive Programmer’s Handbook/Chapter 07: Dynamic Programming/4.Minimum Path Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        for(int i = 1; i < m; ++i) grid[i][0] += grid[i - 1][0];
+        for(int i = 1; i < n; ++i) grid[0][i] += grid[0][i - 1];
+        for(int i = 1; i < m; ++i) {
+            for(int j = 1; j < n; ++j) {
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        return grid[m-1][n-1];
+    }
+};
