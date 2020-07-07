4c29cd4ae0b23a765d38b2e3c47d38403b2225c0
HU <huxixi.love.story@gmail.com>
2020-07-07 10:56:19 +0800

    Create 3.Lowest Common Ancestor of a Binary Tree.cpp

---
+++ Competitive Programmer’s Handbook/Chapter 18: Tree Queries/3.Lowest Common Ancestor of a Binary Tree.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == p || root == q || root == nullptr)
+            return root;
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+        return l == nullptr ? r : (r == nullptr ? l : root);
+    }
+};
