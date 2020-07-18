f831efc1d62d80e20268ef7a9d27475a3dd81c5b
HU <huxixi.love.story@gmail.com>
2020-07-18 13:55:41 +0800

    Update 36两个链表的第一个公共结点.cpp

--- 剑☞Offer系列/题目31-40/36两个链表的第一个公共结点.cpp
+++ 剑☞Offer系列/题目31-40/36两个链表的第一个公共结点.cpp
@@ -1,41 +1,19 @@
-/*
-struct ListNode {
-	int val;
-	struct ListNode *next;
-	ListNode(int x) :
-			val(x), next(NULL) {
-	}
-};*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* FindFirstCommonNode( ListNode* pHead1, ListNode* pHead2) {
-        int l1 = 0, l2 = 0;
-        ListNode* p = pHead1;
-        ListNode* q = pHead2;
-        while(p || q) {
-            if(p) {
-                ++l1;
-                p = p->next;
-            }
-            if(q) {
-                ++l2;
-                q = q->next;
-            }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *p = headA, *q = headB;
+        while(p != q) {
+            p = p == nullptr ? headB : p->next;
+            q = q == nullptr ? headA : q->next;
         }
-        if(l1 > l2) {
-            for(int i = 0; i < l1 - l2; ++i) 
-                pHead1 = pHead1->next;
-        }
-        else {
-            for(int i = 0; i < l2 - l1; ++i) 
-                pHead2 = pHead2->next;
-        }
-        while(pHead1) {
-            if(pHead1 == pHead2)
-                return pHead1;
-            pHead1 = pHead1->next;
-            pHead2 = pHead2->next;
-        }
-        return nullptr;
+        return q;
     }
 };
