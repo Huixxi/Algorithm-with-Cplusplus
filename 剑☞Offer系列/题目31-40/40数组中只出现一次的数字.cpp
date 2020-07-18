a9e94ec2a55311a38476a7a5267d2bbdca88b24d
HU <huxixi.love.story@gmail.com>
2020-07-18 14:28:05 +0800

    Update 40数组中只出现一次的数字.cpp

--- 剑☞Offer系列/题目31-40/40数组中只出现一次的数字.cpp
+++ 剑☞Offer系列/题目31-40/40数组中只出现一次的数字.cpp
@@ -1,48 +1,16 @@
 class Solution {
 public:
-    void FindNumsAppearOnce(vector<int> data,int* num1,int *num2) {
-        /*
-        // 先排个序 O(nlogn)
-        sort(data.begin(), data.end());
-        int flag = 0;
-        if(data[0] != data[1]) {
-            *num1 = data[0];
-            ++flag;
+    vector<int> singleNumber(vector<int>& nums) {
+        int a = 0, idx = 0, x = 0, y = 0;
+        for(auto n : nums) a ^= n;
+        while(!(a & (1 << idx))) ++idx;
+        
+        for(auto n : nums) {
+            if(n & (1 << idx))
+                x ^= n;
+            else 
+                y ^= n;
         }
-        for(int i = 1; i < data.size() - 1; ++i) {
-            if(data[i] != data[i-1] && data[i] != data[i+1]) {
-                if(flag == 0) {
-                    *num1 = data[i];
-                    ++flag;
-                }
-                else if(flag == 1) {
-                    *num2 = data[i];
-                    ++flag;
-                }
-            }
-            if(flag == 2)
-                return;
-        }
-        *num2 = data[data.size() - 1];
-        */
-        // Update
-        map<int, int> mm;
-        for(int& i : data) {
-            if(mm.find(i) != mm.end())
-                ++mm[i];
-            else
-                mm[i] = 1;
-        }
-        int c = 0;
-        for(int& i : data) {
-            if(c == 0 && mm[i] == 1) {
-                *num1 = i;
-                ++c;
-            }
-            else if(c == 1 && mm[i] == 1) {
-                *num2 = i;
-                break;
-            }
-        }
+        return vector<int>{x, y};
     }
 };
