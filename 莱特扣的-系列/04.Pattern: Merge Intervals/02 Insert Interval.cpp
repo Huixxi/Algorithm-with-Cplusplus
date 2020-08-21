9143a1c1627406b36b53a1a0ef8e93f998c61be6
HU <huxixi.love.story@gmail.com>
2020-08-21 13:52:11 +0800

    Update 02 Insert Interval.cpp

--- 莱特扣的-系列/04.Pattern: Merge Intervals/02 Insert Interval.cpp
+++ 莱特扣的-系列/04.Pattern: Merge Intervals/02 Insert Interval.cpp
@@ -2,34 +2,23 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> res;
-        if(intervals.size() == 0) {
-            res.push_back(newInterval);
-            return res;
+        vector<int> tmp;
+        int i = 0;
+        while(i < intervals.size() && intervals[i][1] < newInterval[0]) {
+            res.push_back(intervals[i++]);
         }
-        int flag = 0;
-        if(newInterval[1] < intervals[0][0]) {
-            res.push_back(newInterval);
-            flag = 2;
-        }
-        for(int i = 0; i < intervals.size(); ++i) {
-            if((flag == 0 || flag == 2) && (newInterval[0] > intervals[i][1] || newInterval[1] < intervals[i][0])) {
-                res.push_back(intervals[i]);
-                if(i < intervals.size() - 1 && newInterval[0] > intervals[i][1] && newInterval[1] < intervals[i+1][0])
-                    flag = 1;
-            }
-            else if(flag == 1 && newInterval[0] > intervals[i][1] || newInterval[1] < intervals[i][0]) {
-                res.push_back(newInterval);
-                res.push_back(intervals[i]);
-                flag = 2;
+        tmp = newInterval;
+        for(; i < intervals.size(); ++i) {
+            if(tmp[0] <= intervals[i][1] && tmp[1] >= intervals[i][0]) {
+                tmp[0] = min(tmp[0], intervals[i][0]);
+                tmp[1] = max(tmp[1], intervals[i][1]);
             }
             else {
-                newInterval[0] = min(newInterval[0], intervals[i][0]);
-                newInterval[1] = max(newInterval[1], intervals[i][1]);
-                flag = 1;
+                res.push_back(tmp);
+                tmp = intervals[i];
             }
         }
-        if(flag != 2)
-            res.push_back(newInterval);
+        res.push_back(tmp);
         return res;
     }
 };
