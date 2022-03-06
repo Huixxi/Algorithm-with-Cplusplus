eff8b968772023cb220f3de135cdb1059fd45180
HU <huxixi.love.story@gmail.com>
2022-03-06 17:16:28 +0800

    Update 2022_03_06_lc560 Subarray Sum Equals K.cpp

--- AlgorithmDaily/2022_03_06_lc560 Subarray Sum Equals K.cpp
+++ AlgorithmDaily/2022_03_06_lc560 Subarray Sum Equals K.cpp
@@ -1,6 +1,18 @@
 /*
-LC560. Subarray Sum Equals K
+LC560. Subarray Sum Equals K (https://leetcode.com/problems/subarray-sum-equals-k/)
+"Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k"
 */
 class Solution {
-  
-}
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int sum = 0, res = 0;
+        unordered_map<int, int> mp; mp[0] = 1;
+        for(auto n : nums) {
+            sum += n;
+            if(mp.count(sum - k) != 0)
+                res += mp[sum - k];
+            ++mp[sum];
+        }
+        return res;
+    }
+};
